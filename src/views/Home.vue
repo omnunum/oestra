b56757cfe94d21f6fee9c8573f3be6fcc761d573
Hank Bond <hbond@doximity.com>
2021-02-10 00:09:10 -0500

    move filings to home route

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Filings v-model="filings"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Filings from '@/views/Filings.vue'
+
+var filings = {
+    2019: {
+        "grossIncome": 126_730.64,
+        "withholdings": (6_000 + 2_395.91 + 23),
+        "filingState": "georgia",
+        "filingStatus": "single"
+    }, 
+    2020: {
+        "grossIncome": 127_200,
+        "withholdings": (18_000 + 2_500 + 22),
+        "filingState": "georgia",
+        "filingStatus": "single"
+    },
+    2021: {
+        "grossIncome": 130_800,
+        "withholdings": (18_000 + 2_500 + 22),
+        "filingState": "georgia",
+        "filingStatus": "single"
+    }
+}
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    Filings
+  },
+  data() { return {
+    filings
+  }}
 }
 </script>
